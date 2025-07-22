aef2d4814fe13c9dc51a7209ef2799102812002f
Mário Henrique <mariohenriquelp@gmail.com>
2025-07-22 16:36:13 -0300

    adicionando lógica do container

--- src/config/dependencies.php
+++ src/config/dependencies.php
@@ -1,13 +1,34 @@
 <?php
 use App\Repository\UserRepository;
 use App\Service\UserService;
+use App\Repository\CourseRepository;
+use App\Service\CourseService;
+use App\Repository\TeamRepository;
+use App\Service\TeamService;
 
 return function ($container) {
     $container->set(UserRepository::class, function () {
         return new UserRepository();
     });
 
+    $container->set(CourseRepository::class, function () {
+        return new CourseRepository();
+    });
+
+    $container->set(TeamRepository::class, function () {
+        return new TeamRepository();
+    });
+
     $container->set(UserService::class, function ($container) {
         return new UserService($container->get(UserRepository::class));
     });
+
+    $container->set(CourseService::class, function ($container) {
+        return new CourseService($container->get(CourseRepository::class));
+    });
+
+    $container->set(TeamService::class, function ($container) {
+        return new TeamService($container->get(TeamRepository::class), $container->get(CourseService::class));
+    });
+
 };
